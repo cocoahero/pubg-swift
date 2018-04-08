3692e893cbd65c8336b4d16df3e4a63c1c8152e9
Jonathan Baker <jonathan@jonathanbaker.me>
2018-04-08 18:01:52 -0400

    Add client function for requesting a match by ID.

--- Sources/PUBG/Client.swift
+++ Sources/PUBG/Client.swift
@@ -20,7 +20,48 @@ public class Client {
         self.apiKey = apiKey
     }
 
+    // MARK: - Public
+
+    @discardableResult
+    public func match(withID id: String, region: Region, resultHandler: @escaping (Result<Data>) -> Void) -> URLSessionTask? {
+        return executeRequest(region: region, path: "matches/\(id)", resultHandler: resultHandler)
+    }
+
     // MARK: - Internal
+
+    func executeRequest(region: Region, path: String, parameters: [String: String] = [:], resultHandler: @escaping (Result<Data>) -> Void) -> URLSessionTask? {
+        switch requestWithRegion(region, path: path, parameters: parameters) {
+        case .success(let request):
+            return executeRequest(request, resultHandler: resultHandler)
+        case .failure(let error):
+            resultHandler(.failure(error))
+            return nil
+        }
+    }
+
+    func executeRequest(_ request: URLRequest, resultHandler: @escaping (Result<Data>) -> Void) -> URLSessionTask {
+        let task = URLSession.shared.dataTask(with: request, completionHandler: { data, response, error in
+            if let data = data, let response = response as? HTTPURLResponse {
+                switch response.statusCode {
+                case 200:
+                    resultHandler(.success(data))
+                case 401:
+                    resultHandler(.failure(Error.unauthorized))
+                case 404:
+                    resultHandler(.failure(Error.notFound))
+                case 429:
+                    resultHandler(.failure(Error.rateLimited))
+                default:
+                    resultHandler(.failure(Error.invalidStatusCode(response.statusCode)))
+                }
+            }
+            else {
+                resultHandler(.failure(error ?? Error.unknown))
+            }
+        })
+        task.resume()
+        return task
+    }
 
     func requestWithRegion(_ region: Region, path: String, parameters: [String: String] = [:]) -> Result<URLRequest> {
         var urlComponents = URLComponents(string: "https://api.playbattlegrounds.com/")!
@@ -42,6 +83,10 @@ public class Client {
 extension Client {
     public enum Error: Swift.Error {
         case invalidURL
+        case invalidStatusCode(Int)
+        case unauthorized
+        case notFound
+        case rateLimited
         case unknown
     }
 }
