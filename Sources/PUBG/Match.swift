6ab0316697d3a11bb194a48825a9a000dbde0b7b
Jonathan Baker <jonathan@jonathanbaker.me>
2018-04-11 09:46:33 -0400

    Add Match model.

---
+++ Sources/PUBG/Match.swift
@@ -0,0 +1,16 @@
+//
+//  Match.swift
+//  PUBG-iOS
+//
+//  Created by Jonathan Baker on 4/11/18.
+//  Copyright © 2018 Jonathan Baker. All rights reserved.
+//
+
+import Foundation
+
+public struct Match: Decodable {
+
+    // MARK: - Properties
+
+    public let id: String
+}
