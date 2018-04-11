438326963512d678e3dd40ed48e1b6c7cb3761bf
Jonathan Baker <jonathan@jonathanbaker.me>
2018-04-11 09:46:19 -0400

    Add Player model.

---
+++ Sources/PUBG/Player.swift
@@ -0,0 +1,16 @@
+//
+//  Player.swift
+//  PUBG-iOS
+//
+//  Created by Jonathan Baker on 4/11/18.
+//  Copyright © 2018 Jonathan Baker. All rights reserved.
+//
+
+import Foundation
+
+public struct Player: Decodable {
+
+    // MARK: - Properties
+
+    public let id: String
+}
